d0fed6614773184b9d788f0c62c0d43a02eadda6
mlevitorresv <levitorres66@gmail.com>
2023-12-21 10:34:42 +0100

    ADD: test for login

---
+++ tests/login.test.ts
@@ -0,0 +1,22 @@
+const request = require('supertest');
+import app from '../app'
+
+describe('login tests', () => {
+    it('should return a json with credentials error', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send({ "email": "email", "password": "password" })
+
+        expect(response.statusCode).toEqual(401)
+        expect(response.body).toEqual({ "error": "Unauthorized: Invalid credentials" })
+    })
+
+    it('should return a token', async () => {
+        const response = await request(app)
+            .post('/login')
+            .send({ "email": process.env.USER_ADMIN, "password": process.env.PASSWORD_ADMIN })
+
+        expect(response.statusCode).toEqual(200)
+        expect(response.body).toHaveProperty('token')
+    })
+})
