f12cc3eb3b13a11bc0b0a05377b0fe5c391fe328
mlevitorresv <levitorres66@gmail.com>
2023-12-22 12:16:58 +0100

    FIX: pubblic return in JSON

--- app.ts
+++ app.ts
@@ -4,14 +4,14 @@ import { userRouter } from './controllers/user'
 import { contactRouter } from './controllers/contact'
 import { bookingRouter } from './controllers/booking'
 import { authMiddleware } from './middleware/auth'
-import { showEndpoints } from './public'
+import { publicRouter } from './public'
 import { loginRouter } from './controllers/login'
 const app = express()
 
 app.use(express.json())
 
 app.use('/login', loginRouter)
-app.use('/public', showEndpoints(app))
+app.use('/public', publicRouter)
 
 app.use(authMiddleware);
 
--- public.ts
+++ public.ts
@@ -1,28 +1,11 @@
 import express, { Request, Response } from 'express'
 import expressListEndpoints from 'express-list-endpoints';
+import app from './app';
 
-export const showEndpoints = (app: express.Express) => {
-    const publicRouter = express.Router();
+
+    export const publicRouter = express.Router();
 
     publicRouter.get('/', (req: Request, res: Response) => {
         const endpoints = expressListEndpoints(app);
-        const data = `
-        <!DOCTYPE html>
-        <html lang="es">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Public</title>
-        </head>
-        <body>
-            <h1>MIRANDA HOTEL</h1>
-            <ul>
-                ${endpoints.map((endpoint: any) => `<li>${endpoint.path} : ${endpoint.methods.join(', ')}</li>`).join('')}
-            </ul>
-        </body>
-        </html>
-            `
-        res.send(data)
+        res.json({name: 'Hotel Miranda', endpoints: endpoints})
     })
-    return publicRouter;
-}
