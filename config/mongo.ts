e0121102e76f4d55729391e247bcf1f8b2da4a27
mlevitorresv <levitorres66@gmail.com>
2024-01-03 10:31:05 +0100

    FIX: Atlas config correctly

--- config/mongo.ts
+++ config/mongo.ts
@@ -2,10 +2,15 @@ import mongoose from "mongoose"
 
 const db_user = process.env.DB_USER;
 const db_password = process.env.DB_PASSWORD
+const db_collection = process.env.DB_COLLECTION
+const db_cluster = process.env.DB_CLUSTER
+const db_name = process.env.DB_NAME
+
+
 
 export const mongoConnect = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://${db_user}:${db_password}@hotelmiranda.ukonsrz.mongodb.net/`)
+        await mongoose.connect(`mongodb+srv://${db_user}:${db_password}@${db_collection}.${db_cluster}.mongodb.net/${db_name}`)
         console.log('Success connection...')
     } catch (e) {
         console.error('DB error: ',e)
