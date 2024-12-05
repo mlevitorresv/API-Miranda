7707c6b5830cb4e678748860909e12625d9ee2b6
mlevitorresv <levitorres66@gmail.com>
2024-12-05 09:44:22 +0100

    src movido a /api

--- dist/src/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const port = 4000;
-app_1.app.listen(port, () => {
-    console.log(`app listening on port ${port}`);
-});
