ef12c7676ece2b119eb13ed6b496bb165e87dfbb
mlevitorresv <levitorres66@gmail.com>
2024-12-05 09:46:04 +0100

    eliminados rchivos de aws

--- lambda.ts
+++
@@ -1,5 +0,0 @@
-import { app } from './src/app'
-import serverless from 'serverless-http'
-export const handler = serverless(app, {
-	response: { headers: { 'Access-Control-Allow-Origin': '*' } }
-})
