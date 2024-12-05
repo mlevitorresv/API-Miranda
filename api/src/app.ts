3c0820b250095ebd2b2b8c4ed1d9485db1c8e36f
mlevitorresv <levitorres66@gmail.com>
2024-12-05 10:33:30 +0100

    resueltos problemas de app.lsten()

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,37 +1,37 @@
-import express from 'express'
-import cors from 'cors'
-import { roomRouter } from './controllers/room'
-import { userRouter } from './controllers/user'
-import { contactRouter } from './controllers/contact'
-import { bookingRouter } from './controllers/booking'
-import { authMiddleware } from './middleware/auth'
-import { publicRouter } from './public/public'
-import { loginRouter } from './controllers/login'
-import { mongoConnect } from './config/mongo'
+import express from 'express';
+import cors from 'cors';
+import { roomRouter } from './controllers/room';
+import { userRouter } from './controllers/user';
+import { contactRouter } from './controllers/contact';
+import { bookingRouter } from './controllers/booking';
+import { authMiddleware } from './middleware/auth';
+import { publicRouter } from './public/public';
+import { loginRouter } from './controllers/login';
+import { mongoConnect } from './config/mongo';
 
+export const app = express();
 
-export const app = express()
 const corsOptions = {
-    "origin": "*",
-    "credentials" : true,
-    "methods": "GET,PUT,PATCH,POST,DELETE",
-    "allowedHeaders": "Content-Type,Authorization",
-  }
+  "origin": "*",
+  "credentials": true,
+  "methods": "GET,PUT,PATCH,POST,DELETE",
+  "allowedHeaders": "Content-Type,Authorization",
+};
 
 mongoConnect();
 
-app.use(cors(corsOptions))
-app.use(express.json())
+app.use(cors(corsOptions));
+app.use(express.json());
 
-app.use('/login', loginRouter)
-app.use('/public', publicRouter)
+app.use('/login', loginRouter);
+app.use('/public', publicRouter);
 
 app.use(authMiddleware);
 
-app.use('/rooms', roomRouter)
+app.use('/rooms', roomRouter);
+app.use('/users', userRouter);
+app.use('/contacts', contactRouter);
+app.use('/bookings', bookingRouter);
 
-app.use('/users', userRouter)
-
-app.use('/contacts', contactRouter)
-
-app.use('/bookings', bookingRouter)
+// Exporta la app para que Vercel la maneje automáticamente
+export default app;
